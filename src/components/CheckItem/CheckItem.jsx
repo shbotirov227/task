8a2e92635631aeacb59ab1dbf49deeee152bfb31
shbotirov <shbotirov227@gmail.com>
2022-02-01 03:31:28 +0500

    sdafasdf

--- src/components/CheckItem/CheckItem.jsx
+++ src/components/CheckItem/CheckItem.jsx
@@ -1,46 +1,205 @@
 import React from "react";
 import { Box, Checkbox, FormControlLabel } from "@mui/material";
 import ProductItem from "../ProductItem/ProductItem";
-import CheckItemImg from "../../assets/images/checkItemImg1.png";
+import CheckItemImg1 from "../../assets/images/checkItemImg1.png";
 
 import "./CheckItem.scss";
+
+const data = [
+	{
+		id: 1,
+		img: CheckItemImg1,
+		title: "Bento Matte 3D Illustration",
+		span: "UI design kit",
+		status: "Active",
+		price: "$98",
+		sale: "$3,200",
+		saleStatus: "55.8%",
+		views: "48k",
+		viewsStatus: "8px",
+		likes: "8",
+		likesStatus: "40px",
+	},
+
+	{
+		id: 2,
+		img: CheckItemImg1,
+		title: "Bento Matte 3D Illustration",
+		span: "UI design kit",
+		status: "Active",
+		price: "$48",
+		sale: "$3,200",
+		saleStatus: "37.8%",
+		views: "80k",
+		viewsStatus: "20px",
+		likes: "8",
+		likesStatus: "24px",
+	},
+
+	{
+		id: 3,
+		img: CheckItemImg1,
+		title: "Bento Matte 3D Illustration",
+		span: "UI design kit",
+		status: "Deactive",
+		price: "$98",
+		sale: "$3,200",
+		saleStatus: "27.5%",
+		views: "48k",
+		viewsStatus: "44px",
+		likes: "8",
+		likesStatus: "8px",
+	},
+
+	{
+		id: 4,
+		img: CheckItemImg1,
+		title: "Bento Matte 3D Illustration",
+		span: "UI design kit",
+		status: "Active",
+		price: "$98",
+		sale: "$3,200",
+		saleStatus: "37.8%",
+		views: "24k",
+		viewsStatus: "12px",
+		likes: "8",
+		likesStatus: "44px",
+	},
+
+	{
+		id: 5,
+		img: CheckItemImg1,
+		title: "Bento Matte 3D Illustration",
+		span: "UI design kit",
+		status: "Deactive",
+		price: "$98",
+		sale: "$3,200",
+		saleStatus: "27.5%",
+		views: "20k",
+		viewsStatus: "44px",
+		likes: "8",
+		likesStatus: "24px",
+	},
+
+	{
+		id: 6,
+		img: CheckItemImg1,
+		title: "Bento Matte 3D Illustration",
+		span: "UI design kit",
+		status: "Active",
+		price: "$98",
+		sale: "$3,200",
+		saleStatus: "55.8%",
+		views: "48k",
+		viewsStatus: "8px",
+		likes: "8",
+		likesStatus: "44px",
+	},
+
+	{
+		id: 7,
+		img: CheckItemImg1,
+		title: "Bento Matte 3D Illustration",
+		span: "UI design kit",
+		status: "Deactive",
+		price: "$98",
+		sale: "$3,200",
+		saleStatus: "27.5%",
+		views: "80k",
+		viewsStatus: "44px",
+		likes: "8",
+		likesStatus: "8px",
+	},
+
+	{
+		id: 8,
+		img: CheckItemImg1,
+		title: "Bento Matte 3D Illustration",
+		span: "UI design kit",
+		status: "Active",
+		price: "$98",
+		sale: "$3,200",
+		saleStatus: "37.8%",
+		views: "24k",
+		viewsStatus: "12px",
+		likes: "8",
+		likesStatus: "44px",
+	},
+
+	{
+		id: 9,
+		img: CheckItemImg1,
+		title: "Bento Matte 3D Illustration",
+		span: "UI design kit",
+		status: "Deactive",
+		price: "$98",
+		sale: "$3,200",
+		saleStatus: "27.5%",
+		views: "20k",
+		viewsStatus: "44px",
+		likes: "8",
+		likesStatus: "24px",
+	},
+
+	{
+		id: 10,
+		img: CheckItemImg1,
+		title: "Bento Matte 3D Illustration",
+		span: "UI design kit",
+		status: "Deactive",
+		price: "$98",
+		sale: "$3,200",
+		saleStatus: "27.5%",
+		views: "20k",
+		viewsStatus: "44px",
+		likes: "8",
+		likesStatus: "24px",
+	},
+];
 
 const CheckItem = () => {
 	const [checked, setChecked] = React.useState([true, false]);
 
-	const handleChange1 = (event) => {
+	const handleChange1 = (event) =>
 		setChecked([event.target.checked, event.target.checked]);
-	};
-
-	const handleChange2 = (event) => {
-		setChecked([event.target.checked, checked[1]]);
-	};
-
-	const handleChange3 = (event) => {
+		const handleChange3 = (event) =>
 		setChecked([checked[0], event.target.checked]);
-	};
+		const handleChange2 = (event) =>
+			setChecked([event.target.checked, checked[1]]);
 
 	const children = (
 		<Box sx={{ display: "flex", flexDirection: "column", ml: 3 }}>
-			<div style={{ display: "flex" }}>
-				<FormControlLabel
-					style={{ margin: 0 }}
-					label={
-						<img
-							src={CheckItemImg}
-							className="CheckItem-img"
-							alt=""
+			{data.map((el, index) => {
+				return (
+					<div className="CheckItem-element">
+						<FormControlLabel
+							style={{ margin: 0, position: "absolute" }}
+							label=""
+							control={
+								<Checkbox
+									checked={checked[0]}
+									onChange={handleChange2}
+								/>
+							}
 						/>
-					}
-					control={
-						<Checkbox
-							checked={checked[0]}
-							onChange={handleChange2}
+						<ProductItem
+							key={index}
+							img={el.img}
+							title={el.title}
+							span={el.span}
+							status={el.status}
+							price={el.price}
+							sale={el.sale}
+							saleStatus={el.saleStatus}
+							views={el.views}
+							viewsStatus={el.viewsStatus}
+							likes={el.likes}
+							likesStatus={el.likesStatus}
+							up
 						/>
-					}
-				/>
-				<ProductItem />
-			</div>
+					</div>
+				);
+			})}
 
 			<FormControlLabel
 				label="Child 2"
--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,40 +1,74 @@
 import React from "react";
+import {
+	EditIcon1,
+	EditIcon2,
+	EditIcon3,
+	StatusArrowDown,
+	StatusArrowIcon,
+} from "../../assets/icons/icons";
 
 import "./ProductItem.scss";
 
-const ProductItem = () => {
+const ProductItem = ({
+	title,
+	span,
+	status,
+	img,
+	up,
+	price,
+	down,
+	sale,
+	saleStatus,
+	views,
+	viewsStatus,
+	likes,
+	likesStatus,
+}) => {
 	return (
 		<div className="ProductItem">
-			<div
-				style={{
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "space-between",
-				}}>
+			<div className="el">
+				<img src={img} className="CheckItem-img" alt="" />
 				<div>
-					<h4 className="ProductItem-title">
-						Bento Matte 3D Illustration
-					</h4>
-					<span className="ProductItem-span">UI design kit</span>
+					<h4 className="ProductItem-title">{title}</h4>
+					<span className="ProductItem-span">{span}</span>
 				</div>
 			</div>
 
-			<span className="ProductItem-active">Active</span>
-			<span className="ProductItem-price">$98</span>
+			<div className="ProductItem-actives" style={{ display: "flex" }}>
+				<span className={`ProductItem-${status}`}>{status}</span>
+
+				<div className="ProductItem-editIcons">
+					<EditIcon1 />
+					<EditIcon2 />
+					<EditIcon3 />
+				</div>
+				<span className="ProductItem-price">{price}</span>
+			</div>
 
 			<div className="ProductItem-sales">
-				<span className="ProductItem-sale">$3,200</span>
-				<span className="ProductItem-saleStatus">55.8%</span>
+				<span className="ProductItem-sale">{sale}</span>
+				<span style={{ margin: "0 10px" }}>
+					{up ? (
+						<StatusArrowIcon />
+					) : down ? (
+						<StatusArrowDown />
+					) : undefined}
+				</span>
+				<span className="ProductItem-saleStatus">{saleStatus}</span>
 			</div>
 
 			<div className="ProductItem-views">
-				<span className="ProductItem-sale">48k</span>
-				<div className="ProductItem-viewsStatus"></div>
+				<span className="ProductItem-sale">{views}</span>
+				<div
+					className="ProductItem-viewsStatus"
+					style={{ width: { viewsStatus } }}></div>
 			</div>
 
 			<div className="ProductItem-likes">
-				<span className="ProductItem-sale">8</span>
-				<div className="ProductItem-likesStatus"></div>
+				<span className="ProductItem-sale">{likes}</span>
+				<div
+					className="ProductItem-likesStatus"
+					style={{ width: { likesStatus } }}></div>
 			</div>
 		</div>
 	);
